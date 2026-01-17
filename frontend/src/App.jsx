80f657644552c31404555a43cbfae6ee8c57f444
NaleeshaThanushka <156690991+NaleeshaThanushka@users.noreply.github.com>
2026-01-17 22:02:58 +0530

    started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-500 text-white p-4 text-2xl">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
